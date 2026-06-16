d82edf0219fac59ff43419fa1b1053f4938d40e0
jwburkett3 <jwburkett3@gmail.com>
2026-06-16 14:41:01 -0500

    Force cache bust

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE = 'field-office-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+const CACHE='fo-1781638700';
+self.addEventListener('install',e=>{self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{if(e.request.mode==='navigate'){e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)));}else{e.respondWith(fetch(e.request,{cache:'no-store'}));}
+});
